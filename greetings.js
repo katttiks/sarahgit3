7657599a8c6af3dbf3c272b029c1027781d76c2b
Kartikeya <kartvat3@in.ibm.com>
2017-03-23 16:52:45 +0530

    3rd personna

--- greetings.js
+++ greetings.js
@@ -2,7 +2,7 @@
 var request = require('request');
 var exports = module.exports = {};
 exports.callRAPI = function(query,callback){
-request('https://hello-world-flask-hirsute-martyrdom.mybluemix.net'+ query, function (error, response, body) {
+request('https://hello-world-flask-unschismatical-crooner.mybluemix.net'+ query, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         callback(null,body); //print the list
     }
